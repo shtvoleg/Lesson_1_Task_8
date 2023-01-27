b04c94cd451a25c10b6bf572d05af8f184a4148a
shtv_oleg <ov09122014@mail.ru>
2023-01-27 18:26:27 +0400

    Lesson_1_Task_8

--- Program.cs
+++ Program.cs
@@ -2,14 +2,24 @@
 
 Console.Clear();				                // очистка консоли
 
-Console.WriteLine("Введите число: ");		// ввод числа
+Console.WriteLine( "Введите число: " );		// ввод числа
 int num = Convert.ToInt32(Console.ReadLine());
 
-if ( num < 0)                               // если отрицательное значение
-    Console.WriteLine($"Число должно быть больше 0!");
+if ( num < 0 )                               // если отрицательное значение
+    Console.WriteLine( $"Число должно быть больше 0!" );
 else
-    Console.WriteLine($"Результат: ");
+    Console.WriteLine( $"Результат: " );
 
+// 1-й вариант:                                      // вывод результата
 int i;                                      // вывод результата
-for (i = 2; i < num; i = i+2)
+for ( i = 2; i < num; i = i+2 )
     Console.Write( $"{i}  " );
+
+// 2-й вариант:
+// int i = 1;
+// while ( i < num )
+//     {
+//     if ( i%2 == 0 )
+//         Console.Write( $"{i}  " );   
+//     i++;
+//     }
